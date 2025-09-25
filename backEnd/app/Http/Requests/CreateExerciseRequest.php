7dfa31a24e00e38cd2cf925195daf5c76ccccfa1
Gilberto Azevedo Junior <gilberto.junior@dbseller.com.br>
2025-09-24 21:49:27 -0300

    adicionando campo de detalhe para exercício e séries na validação

--- backEnd/app/Http/Requests/CreateExerciseRequest.php
+++ backEnd/app/Http/Requests/CreateExerciseRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests;
 
 /**
  * @property string $name
+ * @property string|null $exerciseDetail
  * @property array $serie
  */
 class CreateExerciseRequest extends BaseExerciseRequest
@@ -12,10 +13,12 @@ class CreateExerciseRequest extends BaseExerciseRequest
     {
         return [
             'name' => ['required', 'string', 'max:255', 'unique:exercises,exercises_name'],
+            'exerciseDetail' => ['string', 'max:255'],
             'serie' => ['array'],
             'serie.*.weight' => ['required', 'numeric', 'min:1'],
             'serie.*.repetitions' => ['required', 'integer', 'min:1'],
             'serie.*.rest' => ['required', 'integer', 'min:1'],
+            'serie.*.detail' => ['string', 'max:255']
         ];
     }
 
@@ -25,6 +28,10 @@ class CreateExerciseRequest extends BaseExerciseRequest
             'name.required' => 'É necessário informar o nome do exercício!',
             'name.string' => 'O nome do exercício está inválido!',
             'name.max' => 'O nome do exercício deve ter no máximo 255 caracteres!',
+            'name.unique' => 'Já existe um exercício cadastrado com este nome!',
+
+            'exerciseDetail.string' => 'Detalhe de exercício inválido!',
+            'exerciseDetail.max' => 'O detalhe do exercício deve ter no máximo 255 caracteres!',
 
             'serie.array' => 'As informações sobre as séries do exercício estão inválidas!',
             'serie.*.weight.required' => 'É necessário informar o peso para esta série!',
@@ -37,14 +44,29 @@ class CreateExerciseRequest extends BaseExerciseRequest
 
             'serie.*.rest.required' => 'É necessário informar o tempo de descanso após esta série!',
             'serie.*.rest.integer' => 'O tempo de descanso informado após esta série está inválido!',
-            'serie.*.rest.min' => 'O tempo de descanso informado após esta série deve ser no mínimo 1 segundo!'
+            'serie.*.rest.min' => 'O tempo de descanso informado após esta série deve ser no mínimo 1 segundo!',
+
+            'serie.*.detail.string' => 'Detalhe de série inválido!',
+            'serie.*.detail.max' => 'O detalhe da série deve ter no máximo 255 caracteres!'
         ];
     }
 
     protected function prepareForValidation(): void
     {
+        $series = $this->serie;
+
+        if (!empty($series)) {
+            foreach ($series as $key => $serie) {
+                if (!empty($serie['detail'])) {
+                    $series[$key]['detail'] = strip_tags($serie['detail']);
+                }
+            }
+        }
+
         $this->merge([
             'name' => strip_tags($this->name),
+            'exerciseDetail' => strip_tags($this->exerciseDetail),
+            'serie' => $series ?? []
         ]);
     }
 }
